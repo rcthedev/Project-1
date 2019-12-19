20949e59ab47d07ab414aba8c9702ced370e0b56
Chris Stephens <stephens-c2@live.com>
2019-12-18 20:19:04 -0700

    adding carousel function to page and tweaking CSS and Javascript

--- project 1/js/script.js
+++ project 1/js/script.js
@@ -1,4 +1,7 @@
-
+// carousel initializing
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
 
 //Weather Search button listener
 
@@ -26,55 +29,80 @@ function searchcity () {
         let queryURL1 = "https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&city=*" + city;
     
     
-        // this code sends our ajax requests and sets our promise
-        $.ajax({
-            url: queryURL1,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // this line is creating a DIV variable
-            let activityDiv = $("<div>");
-    
-            for (i = 0; i < 15; i++) {
-    
-                let p = $("<p>").text("Attractions:" + response._embedded.events[i].name);
-                let p2 = $("<p>").text(response._embedded.events[i].url);
-                activityDiv.append(p);
-                activityDiv.append(p2);
-            }
-    
-            $("body").append(activityDiv);
+      // this code sends our ajax requests and sets our promise
+    $.ajax({
+        url: queryURL1,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // this line is creating a DIV variable
+        let carousel = $("<div>")
+        .addClass("carousel");
+        
+        let activityDiv = $("<div>");
+       // let newResponse= response._embedded.events.map(function(a){
+            //if (a.name)
+       
+        for (i = 0; i < 5; i++) {
+            //console.log(city);
+            let a= $("<a>");
+            a.addClass("carousel-item").attr("href", response._embedded.events[i].url);
+            let img = $("<img>").attr("src", response._embedded.events[i].images[1].url)
+
+
+
+            console.log(response._embedded.events[i].images[1].url);
+
+
+
+
+            let p = $("<p>").text("Attractions:" + response._embedded.events[i].name);
+            let p2 = $("<p>").text(response._embedded.events[i].url);
+            activityDiv.append(p);
+            activityDiv.append(p2);
+            a.append(img);
+            carousel.append(a);
+            $(".carouselTxt").append(p);
+
+
         }
-    
-        )
-    
-    
-        // callinfg weather
-        let APIKey = "27c1cb8e958edefa8e76e521738fbd32";
-        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
-    
-        //console.log(response)
-        // this code sends our ajax requests and sets our promise
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // this line is creating a DIV variable
-            let weatherDiv = $("<div>");
-            let p = $("<p>").text("Temperature: " + response.list[0].main.temp);
-    
-            weatherDiv.append(p);
-            $("body").append(weatherDiv);
-        }
-    
-        )
+        $("#carouselTkt").append(carousel);
+        $('.carousel').carousel();
+       // $("carousel").append(activityDiv);
+        console.log(activityDiv)
+      //  <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
     }
 
+    )
+
+
+    // callinfg weather
+    let APIKey = "27c1cb8e958edefa8e76e521738fbd32";
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+
+    //console.log(response)
+    // this code sends our ajax requests and sets our promise
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        let weatherDiv = $(".weather");
+    let p = $("<p>").text("Temperature: " + response.list[0].main.temp);
+    let p3 = $("<p>").text(response.city.name);
+
+
+    weatherDiv.append(p);
+    weatherDiv.append(p3);
+
+    
+}
+    )
+}
 
 $("#searchButton").on("click", function (event) { event.preventDefault; searchcity () });
 
- src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js";
+// src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js";
 
 
 
